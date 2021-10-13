2b8bdd87bd69d340e38e97510f0faf4e8922db94
deshudiosh <deshudiosh@gmail.com>
2021-10-13 17:24:19 +0200

    support for `_scale` suffix

--- PyWykladzinyLayout.py
+++ PyWykladzinyLayout.py
@@ -9,26 +9,38 @@ from tkinterdnd2 import TkinterDnD, DND_FILES
 import tkinter as tk
 
 
-def validate_image(arg) -> Path:
+def valid_image(arg) -> Path:
     """ Return if arg is existing file and is jpg or png"""
     p = Path(arg)
     return p if p.is_file() and p.suffix in ['.jpg', '.png'] else None
 
 
 def process_image(p: Path):
-    if validate_image(p) is None:
+    last = p.stem[-1]
+
+    if last == 'a' or last == 'b':
+        scale = p.stem.find('_scale')
+
+        if scale > -1:
+            scale = p.stem[scale:-1]
+            tex_path = Path(p.parent / (p.stem[:-(len(scale)+1)] + p.suffix))
+        else:
+            scale = ""
+            tex_path = Path(p.parent / (p.stem[:-1] + p.suffix))
+
+    else:
         return
 
-    a_path = Path(p.parent / (p.stem + 'a' + p.suffix))
-    b_path = Path(p.parent / (p.stem + 'b' + p.suffix))
+    a_path = Path(p.parent / (tex_path.stem + scale + 'a' + p.suffix))
+    b_path = Path(p.parent / (tex_path.stem + scale + 'b' + p.suffix))
 
-    if validate_image(a_path) and validate_image(b_path):
+    if valid_image(tex_path) and valid_image(a_path) and valid_image(b_path):
         layout = Image.new('RGB', (3840, 2160), (255, 255, 255))
 
         b = 5  # border
         b2 = b*2
 
-        rug = Image.open(p)
+        rug = Image.open(tex_path)
         rug = ImageOps.fit(rug, (1920-b2, 2160-b2))
         rug = ImageOps.expand(rug, border=b, fill='white')
         layout.paste(rug, box=(0, 0))
@@ -48,10 +60,11 @@ def process_image(p: Path):
         text.text((10, layout.height - text_size * 1.2), p.stem, font=ImageFont.truetype("arial.ttf", text_size),
                   fill=(255, 255, 255), stroke_fill=(0, 0, 0), stroke_width=5)
 
-        layout_path = Path(p.parent / (p.stem + "_layout" + p.suffix))
+        layout_path = Path(p.parent / (tex_path.stem + scale + "_layout" + p.suffix))
         layout.save(layout_path, optimize=True, quality=60)
 
 
+# TODO: CAN"T MAKE THIS SHIT TO WORK WHEN FROZEN WITH PYINSTALLER!
 class TkWindow:
     def __init__(self):
         self.window = TkinterDnD.Tk()
@@ -77,7 +90,7 @@ def start(args):
     if len(args) == 0:
         tkw = TkWindow()
     else:
-        images = [validate_image(arg) for arg in args if validate_image(arg) is not None]
+        images = [valid_image(arg) for arg in args if valid_image(arg) is not None]
         [process_image(img) for img in images]
 
 
@@ -89,8 +102,13 @@ def cli(args):
 
 
 def test():
-    # start([r'X:\!Budynki-Xrefy\Warsaw Spire\3d\smieci\WYKLADZINY\2021.09.10 piasek\PIASEK5.jpg'])
-    start([])  # force tkinter app run
+    # start([r'X:\!Budynki-Xrefy\Warsaw Spire\3d\smieci\WYKLADZINY\2021.10.12 grz bumps\bumps_linie1a.jpg',
+    #        r'X:\!Budynki-Xrefy\Warsaw Spire\3d\smieci\WYKLADZINY\2021.10.12 grz bumps\bumps_linie1b.jpg',
+    #        r'X:\!Budynki-Xrefy\Warsaw Spire\3d\smieci\WYKLADZINY\2021.10.12 grz bumps\bumps_linie1_scale2a.jpg',
+    #        r'X:\!Budynki-Xrefy\Warsaw Spire\3d\smieci\WYKLADZINY\2021.10.12 grz bumps\bumps_linie1_scale2b.jpg',
+    #        r'X:\!Budynki-Xrefy\Warsaw Spire\3d\smieci\WYKLADZINY\2021.10.12 grz bumps\bumps_brick1_scale1b.jpg',
+    #        r'X:\!Budynki-Xrefy\Warsaw Spire\3d\smieci\WYKLADZINY\2021.10.12 grz bumps\bumps_brick1_scale2a.jpg'])
+    start([])  # force tkinter window app run
 
 
 if getattr(sys, "frozen", False):  # if frozen with pyinstaller
